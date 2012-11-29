eaf35a2a5e5851c4fa9e7b53805ff23181533b12
Oscar <oscar.pena@deusto.es>
2012-11-29 11:13:29 +0100

    Add employee template added.

--- employee_manager/urls.py
+++ employee_manager/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'$', 'employee_manager.views.index', name = 'index'),
+    url(r'^add/$', 'employee_manager.views.add_employee', name = 'add_employee'),
+    url(r'^eliminar/(\d*)$', 'employee_manager.views.delete_employee', name = 'delete_employee'),
     # (r'^organizaciones/', include('organization_manager.urls')),
     # (r'^personas/', include('employee_manager.urls')),
     # (r'^proyectos/', include('project_manager.urls')),
--- employee_manager/views.py
+++ employee_manager/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 
@@ -8,19 +8,33 @@ from employee_manager.models import *
 from employee_manager.forms import *
 
 def index(request):
-	form = EmployeeForm()
+    employees = Employee.objects.all()
 
-	employees = Employee.objects.all()
+    return render_to_response("employee_manager/index.html", {
+            "employees": employees, 
+        }, 
+        context_instance = RequestContext(request))
 
-	if request.POST:
-		form = EmployeeForm(request.POST)
-		if form.is_valid():
-			return HttpResponseRedirect("/added")
-	else:
-		form = EmployeeForm()
-	
-	return render_to_response("employee_manager/index.html", {
-			"form": form, 
-			"employees": employees, 
-		}, 
-		context_instance = RequestContext(request))
+def add_employee(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect("/added")
+    else:
+        form = EmployeeForm()
+        
+    return render_to_response("employee_manager/add.html", {
+            "form": form,
+        },
+        context_instance = RequestContext(request))
+
+def delete_employee(request, employeeID):
+    e = get_object_or_404(Employee, pk = employeeID)
+    e.delete()
+    employees = Employee.objects.all()
+
+    return render_to_response("employee_manager/index.html", {
+            "employees": employees,
+            "form": form,
+        },
+        context_instance = RequestContext(request))
