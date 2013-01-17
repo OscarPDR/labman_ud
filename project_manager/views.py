17641fb03bb5c93663f42aa16351670a3f3fe9c5
Oscar <oscar.pena@deusto.es>
2013-01-17 14:38:56 +0100

    Proyect form working properly, without complex validations and autocompletes.

--- project_manager/views.py
+++ project_manager/views.py
@@ -7,6 +7,12 @@ from django.http import HttpResponseRedirect
 from project_manager.models import *
 from project_manager.forms import *
 
+from employee_manager.models import *
+from employee_manager.forms import *
+
+from organization_manager.models import *
+from organization_manager.forms import *
+
 def project_index(request):
     projects = Project.objects.all()
 
@@ -23,39 +29,27 @@ def add_project(request):
     assigned_employee_formset = AssignedEmployeeFormSet(instance = Project(), prefix = 'assigned_employee_formset')
     consortium_member_formset = ConsortiumMemberFormSet(instance = Project(), prefix = 'consortium_member_formset')
 
-    if request.method == 'POST':
-        project_form = ProjectForm(request.POST, request.FILES, prefix = 'project_form')
+    if request.POST:
+        project_form = ProjectForm(request.POST, prefix = 'project_form')
         if project_form.is_valid():
             project = project_form.save(commit = False)
 
             funding_program_form = FundingProgramForm(request.POST, instance = project, prefix = 'funding_program_form')
             funding_amount_formset = FundingAmountFormSet(request.POST, instance = project, prefix = 'funding_amount_formset')
-            assigned_employee_formset = AssignedEmployeeFormSet(instance = project, prefix = 'assigned_employee_formset')
-            consortium_member_formset = ConsortiumMemberFormSet(instance = project, prefix = 'consortium_member_formset')
+            assigned_employee_formset = AssignedEmployeeFormSet(request.POST, instance = project, prefix = 'assigned_employee_formset')
+            consortium_member_formset = ConsortiumMemberFormSet(request.POST, instance = project, prefix = 'consortium_member_formset')
 
             cd_p = project_form.cleaned_data
 
-            project_type = cd_p['project_type']
-            title = cd_p['title']
-            description = cd_p['description']
-            homepage = cd_p['homepage']
-            start_year = cd_p['start_year']
-            end_year = cd_p['end_year']
-            status = cd_p['status']
-            currency = cd_p['currency']
-            observations = cd_p['observations']
-
-            project = Project(
-                project_type = project_type.encode('utf-8'),
-                title = title.encode('utf-8'),
-                description = description.encode('utf-8'),
-                homepage = homepage,
-                start_year = start_year,
-                end_year = end_year,
-                status = status.encode('utf-8'),
-                currency = currency.encode('utf-8'),
-                observations = observations.encode('utf-8'),
-            )
+            project.project_type = cd_p['project_type'].encode('utf-8')
+            project.title = cd_p['title'].encode('utf-8')
+            project.description = cd_p['description'].encode('utf-8')
+            project.homepage = cd_p['homepage']
+            project.start_year = cd_p['start_year']
+            project.end_year = cd_p['end_year']
+            project.status = cd_p['status'].encode('utf-8')
+            project.currency = cd_p['currency'].encode('utf-8')
+            project.observations = cd_p['observations'].encode('utf-8')
 
             # if request.FILES['logo']:
             #     project.logo = request.FILES['logo']
@@ -65,68 +59,63 @@ def add_project(request):
             if funding_program_form.is_valid():
                 cd_f = funding_program_form.cleaned_data
 
-                name = cd_f['name']
-                project_code = cd_f['project_code']
-                start_month = cd_f['start_month']
-                start_year = cd_f['start_year']
-                end_month = cd_f['end_month']
-                end_year = cd_f['end_year']
-                concession_year = cd_f['concession_year']
-                geographical_scope = cd_f['geographical_scope']
-
                 funding_program = FundingProgram(
+                    project = project,
                     organization = cd_f['organization'],
-                    name = name.encode('utf-8'),
-                    project_code = project_code.encode('utf-8'),
-                    start_month = start_month,
-                    start_year = start_year,
-                    end_month = end_month,
-                    end_year = end_year,
-                    concession_year = concession_year,
-                    geographical_scope = geographical_scope.encode('utf-8'),
+                    name = cd_f['name'].encode('utf-8'),
+                    project_code = cd_f['project_code'].encode('utf-8'),
+                    start_month = cd_f['start_month'],
+                    start_year = cd_f['start_year'],
+                    end_month = cd_f['end_month'],
+                    end_year = cd_f['end_year'],
+                    concession_year = cd_f['concession_year'],
+                    geographical_scope = cd_f['geographical_scope'].encode('utf-8'),
                 )
-
-                funding_program.project = project
 
                 funding_program.save()
 
-                current_year = start_year
+                current_year = cd_f['start_year']
 
-                if funding_amount_formset.is_valid():
-                    for funding_amount_form in funding_amount_formset:
-                            if (len(funding_amount_form.cleaned_data) > 0) and (current_year <= end_year):
-                                cd_fa = funding_amount_form.cleaned_data
 
-                                funding_amount = FundingAmount(
-                                    amount = cd_fa['amount'],
-                                    year = current_year,
-                                )
+            # # FIXME
+            if funding_amount_formset.is_valid():
+                for funding_amount_form in funding_amount_formset:
+                    print "funding amount de " + str(current_year)
+                    print "hay cleaned data: " + str(len(funding_amount_form.cleaned_data))
+                    print str(current_year) + " < " + str(cd_f['end_year'])
+                    if (len(funding_amount_form.cleaned_data) > 0) and (current_year <= cd_f['end_year']):
+                        print "entra"
+                        cd_fa = funding_amount_form.cleaned_data
 
-                                funding_amount.project = project
+                        funding_amount = FundingAmount(
+                            project = project,
+                            amount = cd_fa['amount'],
+                            year = current_year,
+                        )
 
-                                funding_amount.save()
+                        funding_amount.save()
+                        print "funding_amount.save()"
 
-                                current_year += 1
+                        current_year += 1
 
-                            else:
-                                print "No fundings amounts to save"
+                    else:
+                        print "No fundings amounts to save"
 
             if assigned_employee_formset.is_valid():
                 for assigned_employee_form in assigned_employee_formset:
-                        if (len(assigned_employee_form.cleaned_data) > 0):
-                            cd_ae = assigned_employee_form.cleaned_data
+                    if (len(assigned_employee_form.cleaned_data) > 0):
+                        cd_ae = assigned_employee_form.cleaned_data
 
-                            assigned_employee = AssignedEmployee(
-                                employee = cd_ae['employee'],
-                                role = cd_ae['role'],
-                            )
+                        assigned_employee_form.project = project
+                        assigned_employee_form.employee = cd_ae['employee']
+                        assigned_employee_form.role = cd_ae['role']
 
-                            assigned_employee.project = project
+                        assigned_employee_form.save()
 
-                            assigned_employee.save()
+                    else:
+                        print "No assigned employees to save"
 
-                        else:
-                            print "No assigned employees to save"
+                assigned_employee_formset.save()
 
             if consortium_member_formset.is_valid():
                 for consortium_member_form in consortium_member_formset:
@@ -134,10 +123,9 @@ def add_project(request):
                             cd_cm = consortium_member_form.cleaned_data
 
                             consortium_member = ConsortiumMember(
+                                project = project,
                                 organization = cd_cm['organization'],
                             )
-
-                            consortium_member.project = project
 
                             consortium_member.save()
 
@@ -149,7 +137,7 @@ def add_project(request):
         project_form = ProjectForm(prefix = 'project_form')
         funding_program_form = FundingProgramForm(instance = Project(), prefix = 'funding_program_form')
         funding_amount_formset = FundingAmountFormSet(instance = Project(), prefix = 'funding_amount_formset')
-        assigned_employee_formset = AssignedEmployeeFormSet(instance = Project(), prefix = 'funding_amount_formset')
+        assigned_employee_formset = AssignedEmployeeFormSet(instance = Project(), prefix = 'assigned_employee_formset')
         consortium_member_formset = ConsortiumMemberFormSet(instance = Project(), prefix = 'consortium_member_formset')
 
     return render_to_response("project_manager/add.html", {
