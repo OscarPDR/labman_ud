7d7445400335e6a022769d67437c3a2ac2d7c601
Oscar <oscar.pena@deusto.es>
2013-01-23 12:32:11 +0100

    Described required fields in project model.

--- project_manager/models.py
+++ project_manager/models.py
@@ -64,56 +64,64 @@ def logo_path(self, filename):
     return self.slug + os.path.splitext(filename)[-1]
 
 
+#########################
+# Model: Project
+#########################
+
 class Project(models.Model):
     project_type = models.CharField(
         max_length = 25,
         choices = PROJECT_TYPES,
         default = 'Project',
-        verbose_name = 'Project type'
+        verbose_name = 'Project type *',     # Required
     )
 
     title = models.CharField(
         max_length = 100,
-        verbose_name = 'Title'
+        verbose_name = 'Title *',    # Required
     )
 
     description = models.TextField(
         max_length = 500,
-        verbose_name = 'Description'
+        verbose_name = 'Description *',      #Required
     )
 
     homepage = models.URLField(
         max_length = 150,
-        verbose_name = 'Homepage'
+        verbose_name = 'Homepage',
+        blank = True,
+        null = True,
     )
 
     start_year = models.IntegerField(
         validators = [MinValueValidator(1990), MaxValueValidator(2015)],
-        verbose_name = 'Start year'
+        verbose_name = 'Start year *',      # Required
     )
 
     end_year = models.IntegerField(
         validators = [MinValueValidator(1990), MaxValueValidator(2015)],
-        verbose_name = 'End year'
+        verbose_name = 'End year *',    # Required
     )
 
     status = models.CharField(
         max_length = 25,
         choices = PROJECT_STATUS,
         default = 'NotStarted',
-        verbose_name = 'Status'
+        verbose_name = 'Status *',      # Required
     )
 
     total_funds = models.DecimalField(
         max_digits = 10,
         decimal_places = 2,
         blank = True,
+        null = True,
         verbose_name = 'Total funds',
     )
 
     observations = models.TextField(
         max_length = 500,
-        verbose_name = 'Observations'
+        verbose_name = 'Observations',
+        blank = True,
     )
 
     logo = models.ImageField(
@@ -147,62 +155,79 @@ class Project(models.Model):
     def update(self, *args, **kwargs):
         # You have to prepare what you need before delete the model
         storage = self.logo.storage
-        path = self.logo.path
+        try:
+            path = self.logo.path
+            os.remove(path)
+            # Delete the file after the model
+            storage.delete(path)
+        except:
+            pass
+            # No previous logo
 
-        os.remove(path)
 
-        # Delete the file after the model
-        storage.delete(path)
-
+#########################
+# Model: FundingProgram
+#########################
 
 class FundingProgram(models.Model):
-    project = models.ForeignKey(Project)
-    organization = models.ForeignKey(Organization, verbose_name = 'Funding organization')
+    project = models.ForeignKey(Project, blank = True, null = True,)
+    organization = models.ForeignKey(Organization, verbose_name = 'Funding organization', blank = True, null = True,)
 
     name = models.CharField(
         max_length = 25,
         verbose_name = 'Program name',
+        blank = True,
     )
 
     project_code = models.CharField(
         max_length = 25,
         verbose_name = 'Project code',
+        blank = True,
     )
 
     start_month = models.CharField(
         max_length= 15,
         choices = MONTHS,
         default = '1',
-        verbose_name = 'Start month'
+        verbose_name = 'Start month',
+        blank = True,
     )
 
     start_year = models.IntegerField(
         validators = [MinValueValidator(1990), MaxValueValidator(2015)],
-        verbose_name = 'Start year'
+        verbose_name = 'Start year',
+        blank = True,
+        null = True,
     )
 
     end_month = models.CharField(
         max_length= 15,
         choices = MONTHS,
         default = '12',
-        verbose_name = 'End month'
+        verbose_name = 'End month',
+        blank = True,
     )
 
     end_year = models.IntegerField(
         validators = [MinValueValidator(1990), MaxValueValidator(2015)],
-        verbose_name = 'End year'
+        verbose_name = 'End year',
+        blank = True,
+        null = True,
     )
 
     concession_year = models.IntegerField(
         validators = [MinValueValidator(1990), MaxValueValidator(2015)],
-        verbose_name = 'Concession year'
+        verbose_name = 'Concession year',
+        blank = True,
+        null = True,
     )
 
     geographical_scope = models.CharField(
         max_length = 25,
         choices = GEOGRAPHICAL_SCOPE,
         default = 'Province',
-        verbose_name = 'Geographical scope'
+        verbose_name = 'Geographical scope',
+        blank = True,
     )
 
     # logo = models.ImageField(
@@ -224,13 +249,19 @@ class FundingProgram(models.Model):
     def update(self, *args, **kwargs):
         # You have to prepare what you need before delete the model
         storage = self.logo.storage
-        path = self.logo.path
+        try:
+            path = self.logo.path
+            os.remove(path)
+            # Delete the file after the model
+            storage.delete(path)
+        except:
+            pass
+            # No previous logo
 
-        os.remove(path)
 
-        # Delete the file after the model
-        storage.delete(path)
-
+#########################
+# Model: FundingAmount
+#########################
 
 class FundingAmount(models.Model):
     project = models.ForeignKey(Project)
@@ -239,39 +270,51 @@ class FundingAmount(models.Model):
         max_digits = 10,
         decimal_places = 2,
         verbose_name = 'Amount',
-        blank = True
+        blank = True,
     )
 
     year = models.IntegerField(
         validators = [MinValueValidator(1990), MaxValueValidator(2015)],
         verbose_name = 'Concession year',
-        blank = True
+        blank = True,
     )
 
 
+#########################
+# Model: AssignedEmployee
+#########################
+
 class AssignedEmployee(models.Model):
     project = models.ForeignKey(Project)
-    employee = models.ForeignKey(Employee, verbose_name = "Employee")
+    employee = models.ForeignKey(Employee, verbose_name = "Employee *")     # Required
 
     role = models.CharField(
         max_length = 50,
         choices = ROLES,
         default = 'Researcher',
-        verbose_name = 'Role'
+        verbose_name = 'Role *',    # Required
     )
 
 
-class ConsortiumMember(models.Model):
-    project = models.ForeignKey(Project)
-    organization = models.ForeignKey(Organization, verbose_name = "Organization")
-
-    def __unicode__(self):
-        return u'Project ID %s - Organization ID %s' % (self.project.title, self.organization.id)
-
+#########################
+# Model: ProjectLeader
+#########################
 
 class ProjectLeader(models.Model):
     project = models.ForeignKey(Project)
-    organization = models.ForeignKey(Organization, verbose_name = "Leader organization")
+    organization = models.ForeignKey(Organization, verbose_name = "Leader organization *")      # Required
 
     def __unicode__(self):
         return u'Project ID %s - Leader organization ID %s' % (self.project.title, self.organization.id)
+
+
+#########################
+# Model: ConsortiumMember
+#########################
+
+class ConsortiumMember(models.Model):
+    project = models.ForeignKey(Project)
+    organization = models.ForeignKey(Organization, verbose_name = "Organization *")     # Required
+
+    def __unicode__(self):
+        return u'Project ID %s - Organization ID %s' % (self.project.title, self.organization.id)
