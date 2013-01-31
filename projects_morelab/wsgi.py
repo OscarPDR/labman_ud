c20a98b7f6e9e868258b5f1d7d72645977863493
Oscar Peña <oscar.pena@deusto.es>
2013-01-31 12:47:50 +0100

    Commit on the server.

--- projects_morelab/wsgi.py
+++ projects_morelab/wsgi.py
@@ -13,9 +13,13 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+import os, sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projects_morelab.settings")
+sys.path.append('/home/morelab/projects_morelab')
+sys.path.append('/home/morelab/projects_morelab/projects_morelab')
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projects_morelab.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "projects_morelab.settings"
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
