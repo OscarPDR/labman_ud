adc043a55b5ded96e2a519fd28de7cbcb1b552b5
Oscar Peña <oscar.pena@deusto.es>
2013-01-31 13:04:35 +0100

    Commit from the server.

--- projects_morelab/wsgi.py
+++ projects_morelab/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI DEV config for projects_morelab project.
+WSGI PROD config for projects_morelab project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
