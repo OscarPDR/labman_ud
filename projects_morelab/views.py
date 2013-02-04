0f65403c37d68f3e5b9df25bfe6e31eb2f24b0c8
Oscar <oscar.pena@deusto.es>
2013-02-04 12:27:32 +0100

    Updated login form.

--- projects_morelab/views.py
+++ projects_morelab/views.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
-from django.shortcuts import render_to_response, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render_to_response
+from django.contrib.auth import logout
 
 # Create your views here.
 
@@ -19,24 +19,7 @@ def home(request):
 #########################
 
 
-def logout(request):
+def logout_view(request):
     logout(request)
     # Redirect to a success page.
     return render_to_response('projects_morelab/index.html')
-
-
-#########################
-# View: login
-#########################
-
-
-def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username = username, password = password)
-    if user is not None:
-        if user.is_active:
-            login(request, user)
-            return render_to_response('projects_morelab/index.html')
-    else:
-            return redirect("http://google.es")
