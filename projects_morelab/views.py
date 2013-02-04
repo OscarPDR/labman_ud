a80fc499d1a33eb3376c70e1ea51a1d653d9b0c0
Oscar <oscar.pena@deusto.es>
2013-02-04 11:04:19 +0100

    Second attempt to login

--- projects_morelab/views.py
+++ projects_morelab/views.py
@@ -1,13 +1,14 @@
 # coding: utf-8
 
-from django.shortcuts import render_to_response
-from django.contrib.auth import logout
+from django.shortcuts import render_to_response, redirect
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 #########################
 # View: home
 #########################
+
 
 def home(request):
     return render_to_response('projects_morelab/index.html')
@@ -17,7 +18,25 @@ def home(request):
 # View: logout
 #########################
 
+
 def logout(request):
     logout(request)
     # Redirect to a success page.
     return render_to_response('projects_morelab/index.html')
+
+
+#########################
+# View: login
+#########################
+
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username = username, password = password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return render_to_response('projects_morelab/index.html')
+    else:
+            return redirect("http://google.es")
