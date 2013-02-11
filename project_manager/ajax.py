6e3182f4ed8d11bafddef8be6e04e723ef379aea
Oscar Peña <oscar.pena@deusto.es>
2013-02-11 12:58:30 +0100

    Last fixes

--- project_manager/ajax.py
+++ project_manager/ajax.py
@@ -7,7 +7,7 @@ from funding_call_manager.models import FundingCall
 @dajaxice_register
 def get_funding_call(req, pk):
     dajax = Dajax()
-    fc = FundingCall.objects.get(pk = pk)
+    fc = FundingCall.objects.get(pk = 1)
     dajax.assign('#id_funding_program_form-organization', 'value', fc.organization.id)
     dajax.assign('#id_funding_program_form-full_name', 'value', fc.full_name)
     dajax.assign('#id_funding_program_form-short_name', 'value', fc.short_name)
