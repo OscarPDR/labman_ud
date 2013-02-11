662390fc4db61785efb1df7ef4b704125cdd8437
Oscar Peña <oscar.pena@deusto.es>
2013-02-11 17:59:34 +0100

    commit

--- charts/urls.py
+++ charts/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
-    url(r'^$', 'employee_manager.views.chart_index', name = 'chart_index'),
-    url(r'^incomes_by_year/$', 'employee_manager.views.incomes_by_year', name = 'incomes_by_year'),
+    url(r'^$', 'charts.views.chart_index', name = 'chart_index'),
+    url(r'^incomes_by_year/$', 'charts.views.incomes_by_year', name = 'incomes_by_year'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
