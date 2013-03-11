99059d25c1910f7d7b5476ab9099fe61cf802644
Oscar <oscar.pena@deusto.es>
2013-03-11 14:13:17 +0100

    Fixed charts urls.

--- charts/urls.py
+++ charts/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns('',
     url(r'^total_incomes/$', 'charts.views.total_incomes', name = 'total_incomes'),
     url(r'^incomes/(?P<year>\d{4})/(?P<scope>\S+)$', 'charts.views.incomes_by_year_and_scope', name = 'incomes_by_year_and_scope'),
     url(r'^incomes/(?P<year>\d{4})$', 'charts.views.incomes_by_year', name = 'incomes_by_year'),
+    url(r'^incomes_by_project/$', 'charts.views.incomes_by_project_index', name = 'incomes_by_project_index'),
+    url(r'^incomes_by_project/(?P<project_slug>\S+)$', 'charts.views.incomes_by_project', name = 'incomes_by_project'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
