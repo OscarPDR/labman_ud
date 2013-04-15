262a69e252c36420886edd40b2e0d2bc0c215987
Oscar <oscar.pena@deusto.es>
2013-04-15 16:57:37 +0200

    Added custom admin models.

--- employees/admin.py
+++ employees/admin.py
@@ -1,4 +1,10 @@
+# coding: utf-8
+
 from django.contrib import admin
 from employees.models import Employee
 
-admin.site.register(Employee)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_filter = ['external']
+
+admin.site.register(Employee, EmployeeAdmin)
--- funding_programs/admin.py
+++ funding_programs/admin.py
@@ -1,4 +1,12 @@
+# coding: utf-8
+
 from django.contrib import admin
 from funding_programs.models import FundingProgram
 
-admin.site.register(FundingProgram)
+
+class FundingProgramAdmin(admin.ModelAdmin):
+    search_fields = ['full_name']
+    list_display = ('full_name', 'organization', 'concession_year', 'geographical_scope')
+    list_filter = ['concession_year', 'geographical_scope']
+
+admin.site.register(FundingProgram, FundingProgramAdmin)
