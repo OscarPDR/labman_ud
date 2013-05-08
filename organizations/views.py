2d1500f29fd34003284e902e530bc26b78d0f107
Oscar <oscar.pdr@gmail.com>
2013-05-08 11:56:12 +0200

    Deleted add, edit & delete actions from persons.

--- organizations/views.py
+++ organizations/views.py
@@ -69,40 +69,6 @@ def organization_index(request):
 
 
 #########################
-# View: add_organization
-#########################
-
-@login_required
-def add_organization(request):
-    if request.method == 'POST':
-        form = OrganizationForm(request.POST, request.FILES)
-        if form.is_valid():
-            cd = form.cleaned_data
-
-            organization = Organization(
-                name = cd['name'].encode('utf-8'),
-                country = cd['country'].encode('utf-8'),
-                homepage = cd['homepage'].encode('utf-8'),
-            )
-
-            try:
-                organization.logo = request.FILES['logo']
-            except:
-                pass
-
-            organization.save()
-
-            return HttpResponseRedirect(reverse('organization_index'))
-    else:
-        form = OrganizationForm()
-
-    return render_to_response("organizations/add.html", {
-            'form': form,
-        },
-        context_instance = RequestContext(request))
-
-
-#########################
 # View: organization_info
 #########################
 
@@ -120,58 +86,3 @@ def organization_info(request, slug):
             'projects': projects,
         },
         context_instance = RequestContext(request))
-
-
-#########################
-# View: edit_organization
-#########################
-
-@login_required
-def edit_organization(request, slug):
-    organization = get_object_or_404(Organization, slug = slug)
-
-    if request.method == 'POST':
-        form = OrganizationForm(request.POST, request.FILES)
-        if form.is_valid():
-
-            cd = form.cleaned_data
-
-            organization.name = cd['name'].encode('utf-8')
-            organization.country = cd['country'].encode('utf-8')
-            organization.homepage = cd['homepage'].encode('utf-8')
-
-            try:
-                organization.logo = request.FILES['logo']
-            except:
-                pass
-
-            organization.save()
-
-            return HttpResponseRedirect(reverse('organization_index'))
-    else:
-        data = {
-            'name': organization.name,
-            'country': organization.country,
-            'homepage': organization.homepage,
-            'logo': organization.logo,
-        }
-
-        form = OrganizationForm(initial = data)
-
-    return render_to_response("organizations/edit.html", {
-            'organization': organization,
-            'form': form,
-        },
-        context_instance = RequestContext(request))
-
-
-#########################
-# View: delete_organization
-#########################
-
-@login_required
-def delete_organization(request, slug):
-    organization = get_object_or_404(Organization, slug = slug)
-    organization.delete()
-
-    return HttpResponseRedirect(reverse('organization_index'))
