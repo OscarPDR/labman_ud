caf6734897dfa9356b73af4a0cac2d978a583f11
Oscar Peña <oscar.pdr@gmail.com>
2013-09-19 12:48:53 +0200

    Funding charts fixed to work on deploy version

--- labman_ud/charts/urls.py
+++ labman_ud/charts/urls.py
@@ -2,13 +2,20 @@ from django.conf.urls import patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
-    url(r'^total_incomes/$', 'charts.views.total_incomes', name='total_incomes'),
-    url(r'^total_incomes_by_scope/$', 'charts.views.total_incomes_by_scope', name='total_incomes_by_scope'),
-    url(r'^incomes/(?P<year>\d{4})/(?P<scope>\S+)$', 'charts.views.incomes_by_year_and_scope', name='incomes_by_year_and_scope'),
-    url(r'^incomes/(?P<year>\d{4})$', 'charts.views.incomes_by_year', name='incomes_by_year'),
-    url(r'^incomes_by_project/$', 'charts.views.incomes_by_project_index', name='incomes_by_project_index'),
-    url(r'^incomes_by_project/(?P<project_slug>\S+)$', 'charts.views.incomes_by_project', name='incomes_by_project'),
 
+    url(r'^$', 'charts.views.chart_index', name='chart_index'),
+
+    # /funding
+    url(r'^funding/$', 'charts.views.funding_charts_index', name='funding_charts_index'),
+    
+    url(r'^funding/total_incomes/$', 'charts.views.funding_total_incomes', name='funding_total_incomes'),
+    url(r'^funding/total_incomes_by_scope/$', 'charts.views.funding_total_incomes_by_scope', name='funding_total_incomes_by_scope'),
+    url(r'^funding/incomes/(?P<year>\d{4})/(?P<scope>\S+)$', 'charts.views.funding_incomes_by_year_and_scope', name='funding_incomes_by_year_and_scope'),
+    url(r'^funding/incomes/(?P<year>\d{4})$', 'charts.views.funding_incomes_by_year', name='funding_incomes_by_year'),
+    url(r'^funding/incomes_by_project/$', 'charts.views.funding_incomes_by_project_index', name='funding_incomes_by_project_index'),
+    url(r'^funding/incomes_by_project/(?P<project_slug>\S+)$', 'charts.views.funding_incomes_by_project', name='funding_incomes_by_project'),
+
+    # /publications
     url(r'^number_of_publications/$', 'charts.views.number_of_publications', name='number_of_publications'),
 )
 
