3aeb82c6a8a5cbac96f302c374ebca425f1b79fd
Jon Lazaro <lazaro.jon@gmail.com>
2013-09-20 09:26:18 +0200

    Publication delete and backup logic to utils. Corrected bugs in Journal Articles parsing.

--- labman_ud/generators/zotero_labman/management/commands/reset_publication_db.py
+++ labman_ud/generators/zotero_labman/management/commands/reset_publication_db.py
@@ -11,6 +11,8 @@ from entities.projects.models import RelatedPublication
 from datetime import datetime
 from django.utils.timezone import utc
 
+from generators.zotero_labman.utils import delete_publication
+
 import os
 
 class Command(NoArgsCommand):
@@ -19,31 +21,14 @@ class Command(NoArgsCommand):
     help = 'Removes every publication in labman (saving only relations between publications and projects) and restarts it with data from Zotero.'
 
     def handle_noargs(self, **options):
-        publ_proj = {}
-
         print "[RESET_PUBS] Deleting every publication in DB..."
+
+        backup_dataset = []
+
         for pub in Publication.objects.all():
-
-
-            # Delete PDF files
-            if pub.pdf:
-                try:
-                    os.remove(getattr(settings, 'MEDIA_ROOT', None) + '/' + str(pub.pdf))
-                except:
-                    pass
-
-            # Save relations between publications and projects
-            if pub.projects.all():
-                try:
-                    zot_key = ZoteroLog.objects.filter(publication=pub).order_by('-created')[0].zotero_key
-                    publ_proj[zot_key] = []
-                    for proj in pub.projects.all():
-                        publ_proj[zot_key].append(proj)
-                except IndexError:
-                    pass
-
-            # Delete publication objects
-            pub.delete()
+            backup_data = delete_publication(pub)
+            if backup_data:
+                backup_dataset.append(backup_data)
 
         # Generate a log specifying a Zotero library version equal to 0, with the aim of re-syncing all publications
         zotlog = ZoteroLog(zotero_key='-RESYNC-', updated=datetime.utcnow().replace(tzinfo=utc), version=0, observations='')
@@ -52,17 +37,3 @@ class Command(NoArgsCommand):
         # Sync again the library with data from Zotero (calling the sync_zotero command)
         print "\n[RESET_PUBS] Re-syncing with Zotero..."
         call_command('sync_zotero')
-
-        print "\n\n[RESET_PUBS] Re-saving relations between publications and projects..."
-        if publ_proj:
-            for zot_key, projects in publ_proj.items():
-                try:
-                    pub = ZoteroLog.objects.filter(zotero_key=zot_key).order_by('-created')[0].publication
-                    for proj in projects:
-                        relpub = RelatedPublication(publication=pub, project=proj)
-                        relpub.save()
-                        print "\n[RESET_PUBS] PUB ", str(pub), " WITH PROJ: " + str(proj)
-                except IndexError:
-                    pass
-        else:
-            print "\n[RESET_PUBS] No relations to sync."
