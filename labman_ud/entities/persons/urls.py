b78ec3e2b8bc7cbf49b21b4c10c765db6971edc1
Oscar Peña <oscar.pdr@gmail.com>
2013-10-31 09:03:24 +0100

    Update konami positions in member index page

--- labman_ud/entities/persons/urls.py
+++ labman_ud/entities/persons/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^info/(\S+)$', 'entities.persons.views.person_info', name='person_info'),
     url(r'^members/(?P<member_slug>\S+)/$', 'entities.persons.views.member_info', name='member_info'),
     url(r'^members/$', 'entities.persons.views.members', name='members'),
+    url(r'^former_members/(?P<former_member_slug>\S+)/$', 'entities.persons.views.former_member_info', name='former_member_info'),
     url(r'^former_members/$', 'entities.persons.views.former_members', name='former_members'),
 )
 
