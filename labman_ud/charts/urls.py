44be2797675cc7fe7b7bc1c658e64f9ba29d2443
Oscar Peña <oscar.pdr@gmail.com>
2013-11-05 14:37:57 +0100

    Updated index page

--- labman_ud/charts/urls.py
+++ labman_ud/charts/urls.py
@@ -6,8 +6,6 @@ urlpatterns = patterns('',
     url(r'^$', 'charts.views.chart_index', name='chart_index'),
 
     # /funding
-    url(r'^funding/$', 'charts.views.funding_charts_index', name='funding_charts_index'),
-    
     url(r'^funding/total_incomes/$', 'charts.views.funding_total_incomes', name='funding_total_incomes'),
     url(r'^funding/total_incomes_by_scope/$', 'charts.views.funding_total_incomes_by_scope', name='funding_total_incomes_by_scope'),
     url(r'^funding/incomes/(?P<year>\d{4})/(?P<scope>\S+)$', 'charts.views.funding_incomes_by_year_and_scope', name='funding_incomes_by_year_and_scope'),
@@ -16,15 +14,11 @@ urlpatterns = patterns('',
     url(r'^funding/incomes_by_project/(?P<project_slug>\S+)$', 'charts.views.funding_incomes_by_project', name='funding_incomes_by_project'),
 
     # /publications
-    url(r'^publications/$', 'charts.views.publications_charts_index', name='publications_charts_index'),
-    
     url(r'^publications/coauthorship/$', 'charts.views.publications_coauthorship', name='publications_coauthorship'),
     url(r'^publications/morelab_coauthorship/$', 'charts.views.publications_morelab_coauthorship', name='publications_morelab_coauthorship'),
     url(r'^publications/total_number/$', 'charts.views.publications_number_of_publications', name='publications_number_of_publications'),
 
-    # /projects
-    url(r'^projects/$', 'charts.views.projects_charts_index', name='projects_charts_index'),
-    
+    # /projects    
     url(r'^projects/collaborations/$', 'charts.views.projects_collaborations', name='projects_collaborations'),
     url(r'^projects/morelab_collaborations/$', 'charts.views.projects_morelab_collaborations', name='projects_morelab_collaborations'),
     
