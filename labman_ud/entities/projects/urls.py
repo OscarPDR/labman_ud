f3e2476cd5abb68b64efbebdca95fe886af8c755
Oscar Peña <oscar.pdr@gmail.com>
2013-11-05 16:16:02 +0100

    Updated project index templates with workmates' suggestions

--- labman_ud/entities/projects/urls.py
+++ labman_ud/entities/projects/urls.py
@@ -6,8 +6,9 @@ urlpatterns = patterns('',
     url(r'^info/(\S+)$', 'entities.projects.views.project_info', name='project_info'),
     url(r'^email/(\S+)$', 'entities.projects.views.email_project', name='email_project'),
 
-    url(r'^tag/(?P<tag_slug>\S+)/$', 'entities.projects.views.view_project_tag', name='view_project_tag'),
-    url(r'^publication_type/(?P<project_type_slug>\S+)/$', 'entities.projects.views.view_project_type', name='view_project_type'),
+    url(r'^tag/(?P<tag_slug>\S+)/$', 'entities.projects.views.project_index', name='view_project_tag'),
+    url(r'^status/(?P<status_slug>\S+)/$', 'entities.projects.views.project_index', name='view_project_status'),
+    url(r'^project_type/(?P<project_type_slug>\S+)/$', 'entities.projects.views.project_index', name='view_project_type'),
 
     url(r'^tag_cloud/$', 'entities.projects.views.project_tag_cloud', name='project_tag_cloud'),
 )
