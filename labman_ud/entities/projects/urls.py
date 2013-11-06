516bcdc35ce934f539f2aa2ee74dc291eab35091
Oscar <oscar.pdr@gmail.com>
2013-11-06 09:22:07 +0100

    Updated publication_index and search feature, updating changes in projects too.

--- labman_ud/entities/projects/urls.py
+++ labman_ud/entities/projects/urls.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from django.conf.urls import patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
--- labman_ud/entities/publications/urls.py
+++ labman_ud/entities/publications/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('',
     url(r'^$', 'entities.publications.views.publication_index', name='publication_index'),
     url(r'^info/(\S+)$', 'entities.publications.views.publication_info', name='publication_info'),
 
-    url(r'^tag/(?P<tag_slug>\S+)/$', 'entities.publications.views.view_publication_tag', name='view_publication_tag'),
-    url(r'^publication_type/(?P<publication_type_slug>\S+)/$', 'entities.publications.views.view_publication_type', name='view_publication_type'),
+    url(r'^tag/(?P<tag_slug>\S+)/$', 'entities.publications.views.publication_index', name='view_publication_tag'),
+    url(r'^publication_type/(?P<publication_type_slug>\S+)/$', 'entities.publications.views.publication_index', name='view_publication_type'),
 
     url(r'^tag_cloud/$', 'entities.publications.views.publication_tag_cloud', name='publication_tag_cloud'),
 )
