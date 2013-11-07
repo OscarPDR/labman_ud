d5b822aed7b9c090efdfb80b27bdd5beb6fe6e8a
Jon Lazaro <lazaro.jon@gmail.com>
2013-11-07 12:14:21 +0100

    New south management scripts

--- labman_ud/labman_ud/scripts/south_migrate.sh
+++ labman_ud/labman_ud/scripts/south_migrate.sh
@@ -3,5 +3,5 @@
 declare -a apps=( 'events' 'funding_programs' 'organizations' 'persons' 'projects' 'publications' 'utils' 'news' 'zotero_labman')
 
 for app in ${apps[*]}; do
-    python manage.py convert_to_south $app --settings=labman_ud.settings.dev
+    python manage.py migrate $app --settings=labman_ud.settings.dev
 done
