b7044c72d7d1c8e1a42e2a68e05897bbca10bb33
Jon Lazaro <lazaro.jon@gmail.com>
2013-11-07 12:38:51 +0100

    Added news to fixtures management scripts

--- labman_ud/labman_ud/scripts/delete_fixtures.sh
+++ labman_ud/labman_ud/scripts/delete_fixtures.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a apps=( 'events' 'funding_programs' 'organizations' 'persons' 'projects' 'publications' 'utils' )
+declare -a apps=( 'events' 'funding_programs' 'organizations' 'persons' 'projects' 'publications' 'news' 'utils' )
 
 for app in ${apps[*]}; do
     rm entities/$app/fixtures/initial_data.json
