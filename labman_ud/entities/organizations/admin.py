0d9cd312319b3ebe6dbf1dde515c9bf212e5ce85
Oscar Peña <oscar.pdr@gmail.com>
2013-11-08 15:48:27 +0100

    Fixing format and code legibility in organizations module

--- labman_ud/entities/organizations/admin.py
+++ labman_ud/entities/organizations/admin.py
@@ -1,24 +1,26 @@
 # coding: utf-8
 
 from django.contrib import admin
+
 from .models import Organization, OrganizationType, OrganizationLogo
 
 
-#########################
+###########################################################################
 # Class: OrganizationLogoInline
-#########################
+###########################################################################
 
 class OrganizationLogoInline(admin.StackedInline):
     model = OrganizationLogo
     extra = 1
 
 
-#########################
+###########################################################################
 # Class: OrganizationAdmin
-#########################
+###########################################################################
 
 class OrganizationAdmin(admin.ModelAdmin):
     model = Organization
+
     search_fields = ['full_name', 'short_name']
     list_display = ['full_name', 'short_name', 'organization_type']
     list_filter = ['country__full_name', 'organization_type__name']
@@ -28,19 +30,22 @@ class OrganizationAdmin(admin.ModelAdmin):
     ]
 
 
-#########################
+###########################################################################
 # Class: OrganizationTypeAdmin
-#########################
+###########################################################################
 
 class OrganizationTypeAdmin(admin.ModelAdmin):
     model = OrganizationType
+
     list_display = ['name', 'description']
     exclude = ['slug']
 
 
-##################################################
+###########################################################################
+###########################################################################
 # Register classes
-##################################################
+###########################################################################
+###########################################################################
 
 admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(OrganizationType, OrganizationTypeAdmin)
