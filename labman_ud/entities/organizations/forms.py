51331e7dc0f3a6214db4ac3bbea73084145fc5eb
Oscar Peña <oscar.pdr@gmail.com>
2013-11-08 15:48:27 +0100

    Fixing format and code legibility in organizations module

--- labman_ud/entities/organizations/forms.py
+++ labman_ud/entities/organizations/forms.py
@@ -5,33 +5,34 @@ from django.forms import widgets
 
 from .models import Organization
 
+
 # Create your forms here.
 
 
-#########################
+###########################################################################
 # Widget: URLInput
-#########################
+###########################################################################
 
 class URLInput(widgets.Input):
     input_type = 'url'
 
 
-#########################
+###########################################################################
 # Class: FundingProgramSearchForm
-#########################
+###########################################################################
 
 class OrganizationSearchForm(forms.Form):
     text = forms.CharField(max_length=50)
 
 
-#########################
+###########################################################################
 # Form: OrganizationForm
-#########################
+###########################################################################
 
 class OrganizationForm(forms.ModelForm):
     class Meta:
         model = Organization
         widgets = {
-            'homepage': URLInput(attrs = {'placeholder': 'http://'}),
+            'homepage': URLInput(attrs={'placeholder': 'http://'}),
         }
         exclude = ('slug')
--- labman_ud/entities/organizations/views.py
+++ labman_ud/entities/organizations/views.py
@@ -1,36 +1,34 @@
 # coding: utf-8
 
+from django.conf import settings
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.template.defaultfilters import slugify
 
-from django.conf import settings
 
-from django.contrib.auth.decorators import login_required
-
+from .forms import OrganizationSearchForm
 from .models import Organization, OrganizationLogo
-from .forms import OrganizationForm, OrganizationSearchForm
 
 from entities.projects.models import Project, ConsortiumMember
-from entities.projects.forms import *
+
 
 # Create your views here.
+
 
 PAGINATION_NUMBER = settings.ORGANIZATIONS_PAGINATION
 
 
-#########################
+###########################################################################
 # View: organization_index
-#########################
+###########################################################################
 
 def organization_index(request):
     organizations = Organization.objects.all().order_by('full_name')
 
     if request.method == 'POST':
         form = OrganizationSearchForm(request.POST)
+
         if form.is_valid():
             query = form.cleaned_data['text']
             query = slugify(query)
@@ -61,30 +59,26 @@ def organization_index(request):
         # If page is out of range (e.g. 9999), deliver last page of results.
         organizations = paginator.page(paginator.num_pages)
 
-    return render_to_response("organizations/index.html", {
-            'organizations': organizations,
-            'form': form,
-        },
-        context_instance = RequestContext(request))
+    # dictionary to be returned in render_to_response()
+    return_dict = {
+        'form': form,
+        'organizations': organizations,
+    }
+
+    return render_to_response("organizations/index.html", return_dict, context_instance=RequestContext(request))
 
 
-#########################
+###########################################################################
 # View: organization_info
-#########################
+###########################################################################
 
 def organization_info(request, slug):
-    from_page = ''
-
-    http_referer = request.META['HTTP_REFERER']
-
-    if '?page=' in http_referer:
-        from_page = http_referer[http_referer.rfind('/')+1:]
-
     organization = get_object_or_404(Organization, slug=slug)
 
     try:
         organization_logo = OrganizationLogo.objects.get(organization=organization.id)
         logo = organization_logo.logo
+
     except:
         logo = None
 
@@ -93,11 +87,11 @@ def organization_info(request, slug):
     consortium_ids = ConsortiumMember.objects.filter(organization_id=organization.id).values('project_id')
     projects = Project.objects.filter(id__in=consortium_ids).order_by('full_name')
 
-    return render_to_response("organizations/info.html", {
-            'organization': organization,
-            'projects_leaded': projects_leaded,
-            'projects': projects,
-            'from_page': from_page,
-            'logo': logo,
-        },
-        context_instance = RequestContext(request))
+    return_dict = {
+        'logo': logo,
+        'organization': organization,
+        'projects': projects,
+        'projects_leaded': projects_leaded,
+    }
+
+    return render_to_response("organizations/info.html", return_dict, context_instance=RequestContext(request))
