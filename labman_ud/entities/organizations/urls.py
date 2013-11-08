570bc14e7ca65ffb62fa19b39246f6c108dec157
Oscar Peña <oscar.pdr@gmail.com>
2013-11-08 16:24:55 +0100

    Updated organization templates to match the rest of the project's functionality

--- labman_ud/entities/organizations/urls.py
+++ labman_ud/entities/organizations/urls.py
@@ -5,8 +5,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     url(r'^$', 'entities.organizations.views.organization_index', name='organization_index'),
-    url(r'^(?P<publication_type_slug>\S+)/$', 'entities.organizations.views.organization_index', name='organization_index'),
     url(r'^info/(\S+)$', 'entities.organizations.views.organization_info', name='organization_info'),
+
+    url(r'^organization_type/(?P<organization_type_slug>\S+)/$', 'entities.organizations.views.organization_index', name='view_organization_type'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
