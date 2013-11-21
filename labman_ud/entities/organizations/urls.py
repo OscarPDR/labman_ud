6e091421d0649720265b2586a820c3ecc58c263e
Oscar Peña <oscar.pdr@gmail.com>
2013-11-21 10:21:30 +0100

    Fixed organizations search & pagination features. Closes #44

--- labman_ud/entities/organizations/urls.py
+++ labman_ud/entities/organizations/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns('',
     url(r'^$', 'entities.organizations.views.organization_index', name='organization_index'),
     url(r'^info/(\S+)$', 'entities.organizations.views.organization_info', name='organization_info'),
 
+    url(r'^query/(?P<query_string>\D+)/$', 'entities.organizations.views.organization_index', name='view_organization_query'),
+
     url(r'^organization_type/(?P<organization_type_slug>\S+)/$', 'entities.organizations.views.organization_index', name='view_organization_type'),
 )
 
