6081636051ebf5f3042f0a60921d7e7f5684fcf2
Oscar Peña <oscar.pdr@gmail.com>
2013-11-21 18:04:34 +0100

    Fixed news templates.

--- labman_ud/entities/news/views.py
+++ labman_ud/entities/news/views.py
@@ -1,40 +1,36 @@
 # coding: utf-8
 
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.template.defaultfilters import slugify
 
-from entities.news.models import News, PersonRelatedToNews, ProjectRelatedToNews, PublicationRelatedToNews
+from .models import News, PersonRelatedToNews, ProjectRelatedToNews, PublicationRelatedToNews
 
 from entities.persons.models import Person
-
 from entities.projects.models import Project
-
 from entities.publications.models import Publication
 
 
 # Create your views here.
 
 
-#########################
+###########################################################################
 # View: news_index
-#########################
+###########################################################################
 
 def news_index(request):
     news = News.objects.all().order_by('-created')
 
-    return render_to_response('news/index.html', {
-            'news': news,
-        },
-        context_instance=RequestContext(request))
+    # dictionary to be returned in render_to_response()
+    return_dict = {
+        'news': news,
+    }
+
+    return render_to_response('news/index.html', return_dict, context_instance=RequestContext(request))
 
 
-#########################
+###########################################################################
 # View: view_news
-#########################
+###########################################################################
 
 def view_news(request, news_slug):
     news = News.objects.get(slug=news_slug)
@@ -49,11 +45,19 @@ def view_news(request, news_slug):
     related_publications_ids = PublicationRelatedToNews.objects.filter(news=news.id).values('publication_id')
     related_publications = Publication.objects.filter(id__in=related_publications_ids).order_by('slug')
 
-    return render_to_response('news/info.html', {
-            'news': news,
-            # 'tags': tags,
-            'related_persons': related_persons,
-            'related_projects': related_projects,
-            'related_publications': related_publications,
-        },
-        context_instance=RequestContext(request))
+    if not related_persons and not related_projects and not related_publications:
+        related = False
+    else:
+        related = True
+
+    # dictionary to be returned in render_to_response()
+    return_dict = {
+        # 'tags': tags,
+        'news': news,
+        'related': related,
+        'related_persons': related_persons,
+        'related_projects': related_projects,
+        'related_publications': related_publications,
+    }
+
+    return render_to_response('news/info.html', return_dict, context_instance=RequestContext(request))
