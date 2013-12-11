980899b01bf718237692d657cf376357b162ba6c
Oscar <oscar.pdr@gmail.com>
2013-12-11 09:37:43 +0100

    Updated generate_fixtures scripts

--- labman_ud/labman_ud/scripts/generate_fixtures.sh
+++ labman_ud/labman_ud/scripts/generate_fixtures.sh
@@ -5,9 +5,8 @@ declare -a apps=( 'events' 'funding_programs' 'news' 'organizations' 'persons' '
 today=$(date +"%m_%d_%Y")
 
 for app in ${apps[*]}; do
-    mkdir -p entities/$app/fixtures
     if [ -f entities/$app/fixtures/initial_data.json ]; then
         mv entities/$app/fixtures/initial_data.json entities/$app/fixtures/initial_data_$today.json
     fi
-    python manage.py dumpdata $app --indent=4 > entities/$app/fixtures/initial_data.json --settings=labman_ud.settings.dev
+    python manage.py dumpdata $app --indent=4 > entities/$app/fixtures/initial_data.json --settings=labman_ud.settings.prod
 done
