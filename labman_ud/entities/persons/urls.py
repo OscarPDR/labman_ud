20ed674e3a165cc5a5f7d47579ee3b878f64047a
Oscar <oscar.pdr@gmail.com>
2014-03-24 09:01:31 +0100

    Added function to determine a member status (active, former, or nothing)

--- labman_ud/entities/persons/urls.py
+++ labman_ud/entities/persons/urls.py
@@ -6,7 +6,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = patterns('',
     url(r'^$', 'entities.persons.views.person_index', name='person_index'),
     url(r'^query/(?P<query_string>\D+)/$', 'entities.persons.views.person_index', name='view_person_query'),
-    url(r'^info/(\S+)$', 'entities.persons.views.person_info', name='person_info'),
+
+    url(r'^person_info/(?P<person_slug>\S+)/$', 'entities.persons.views.determine_person_info', name='determine_person_info'),
+
+    url(r'^info/(?P<person_slug>\S+)/$', 'entities.persons.views.person_info', name='person_info'),
 
     url(r'^members/org/(?P<organization_slug>\S+)/$', 'entities.persons.views.members', name='members_by_organization'),
     url(r'^members/(?P<member_slug>\S+)/projects/(?P<role_slug>\S+)/$', 'entities.persons.views.member_projects', name='member_projects'),
