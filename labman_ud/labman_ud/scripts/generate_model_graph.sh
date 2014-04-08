1e985ad51fd1035238e3c5394a2678adff3f1545
Oscar <oscar.pdr@gmail.com>
2014-04-08 17:53:58 +0200

    Updated model graph

--- labman_ud/labman_ud/scripts/generate_model_graph.sh
+++ labman_ud/labman_ud/scripts/generate_model_graph.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python manage.py graph_models events funding_programs news organizations persons projects publications utils publications utils -g -o labman_ud_models.png --settings=labman_ud.settings.dev
+python manage.py graph_models events funding_programs news organizations persons projects publications utils -g -o labman_ud_models.png --settings=labman_ud.settings.local
