eb3e306ed4e84cdc994fcc8be97bbbbfdabec458
Pablo Orduña <pablo.orduna@deusto.es>
2014-04-10 16:38:26 +0200

    Add feeds for news

--- labman_ud/entities/news/urls.py
+++ labman_ud/entities/news/urls.py
@@ -3,9 +3,12 @@
 from django.conf.urls import patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from entities.news.views import LatestNewsFeed
+
 urlpatterns = patterns('',
     url(r'^$', 'entities.news.views.news_index', name='news_index'),
     url(r'^view/(?P<news_slug>\S+)/$', 'entities.news.views.view_news', name='view_news'),
+    url(r'^feed/$', LatestNewsFeed(), name='news_feed')
 )
 
 urlpatterns += staticfiles_urlpatterns()
