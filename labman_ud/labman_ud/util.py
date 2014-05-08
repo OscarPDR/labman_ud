f4263ad6c344c0d5e638fed6d37e1c3b7164e074
Pablo Orduña <pablo.orduna@deusto.es>
2014-05-08 15:29:36 +0200

    Add labman_ud.util

---
+++ labman_ud/labman_ud/util.py
@@ -0,0 +1,8 @@
+class FeedWrapper(object):
+    def __init__(self, feed_instance):
+        self.feed_instance = feed_instance
+
+    def __call__(self, request, *args, **kwargs):
+        response = self.feed_instance(request, *args, **kwargs)
+        response['Content-Type'] = 'application/xml; charset=utf-8'
+        return response
