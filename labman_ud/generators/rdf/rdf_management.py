689b0079915fc6994e219cab7af7ab99f1deb4d7
Oscar <oscar.pdr@gmail.com>
2014-06-25 16:49:25 +0200

    Create/Update/Delete general methods included

--- labman_ud/generators/rdf/rdf_management.py
+++ labman_ud/generators/rdf/rdf_management.py
@@ -3,8 +3,13 @@
 
 import urllib
 import urllib2
+from rdflib import URIRef
 
 from django.conf import settings
+
+
+def resource_uri_for_person_from_slug(slug):
+    return URIRef('%s/%s/%s' % (getattr(settings, 'RESOURCES_BASE_URL', None), 'people', slug))
 
 
 def insert_by_post(graph):
@@ -15,6 +20,33 @@ def insert_by_post(graph):
         triples += triple
 
     query = 'INSERT IN GRAPH <%s> { %s }' % (getattr(settings, 'GRAPH_BASE_URL', None), triples.encode('utf-8'))
+
+    data = urllib.urlencode({'query': query})
+    request = urllib2.Request(getattr(settings, 'SPARQL_ENDPOINT_URL', None), data)
+    urllib2.urlopen(request)
+
+
+def delete_resource(resource_uri):
+    query = """
+        WITH <%s>
+        DELETE { <%s> ?p ?o }
+        WHERE {
+            <%s> ?p ?o .
+        }""" % (getattr(settings, 'GRAPH_BASE_URL', None), resource_uri, resource_uri)
+
+    data = urllib.urlencode({'query': query})
+    request = urllib2.Request(getattr(settings, 'SPARQL_ENDPOINT_URL', None), data)
+    urllib2.urlopen(request)
+
+
+def update_resource_uri(old_resource_uri, new_resource_uri):
+    query = """
+        WITH <%s>
+        DELETE { ?s ?p <%s> }
+        INSERT { ?s ?p <%s> }
+        WHERE {
+            ?s ?p <%s> .
+        }""" % (getattr(settings, 'GRAPH_BASE_URL', None), old_resource_uri, new_resource_uri, old_resource_uri)
 
     data = urllib.urlencode({'query': query})
     request = urllib2.Request(getattr(settings, 'SPARQL_ENDPOINT_URL', None), data)
