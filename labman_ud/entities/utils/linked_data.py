65ad157caed037443e9512206360d798cd85ad34
Oscar <oscar.pdr@gmail.com>
2014-06-30 09:13:48 +0200

    Network to RDF

--- labman_ud/entities/utils/linked_data.py
+++ labman_ud/entities/utils/linked_data.py
@@ -56,7 +56,8 @@ def save_geographical_scope_as_rdf(geographical_scope):
     graph.add((resource_uri, DC.title, Literal(geographical_scope.name)))
 
     # Description is optional
-    graph.add((resource_uri, DC.description, Literal(geographical_scope.description)))
+    if geographical_scope.description:
+        graph.add((resource_uri, DC.description, Literal(geographical_scope.description)))
 
     insert_by_post(graph)
 
@@ -84,12 +85,46 @@ def save_tag_as_rdf(tag):
     graph.add((resource_uri, MUTO.tagLabel, Literal(tag.name)))
 
     # Sub Tag Of is optional
-    # graph.add((resource_uri, ???, Literal(tag.sub_tag_of)))
+    if tag.sub_tag_of:
+        graph.add((resource_uri, DCTERMS.isPartOf, Literal(tag.sub_tag_of)))
 
     insert_by_post(graph)
 
 
 def delete_tag_rdf(tag):
     resource_uri = resource_uri_for_tag_from_slug(tag.slug)
+
+    delete_resource(resource_uri)
+
+
+###########################################################################
+# Model: Network
+###########################################################################
+
+def save_network_as_rdf(network):
+    graph = create_namespaced_graph()
+
+    resource_uri = resource_uri_for_network_from_slug(network.slug)
+
+    # Define type and label of resource
+    graph.add((resource_uri, RDF.type, FOAF.OnlineAccount))
+    graph.add((resource_uri, RDFS.label, Literal(network.name)))
+
+    # Name is required
+    graph.add((resource_uri, DC.title, Literal(network.name)))
+
+    # Base URL is required
+    graph.add((resource_uri, FOAF.accountServiceHomepage, Literal(network.base_url)))
+
+    # Icon is optional
+    if network.icon:
+        network_icon_url = '%s%s' % (getattr(settings, 'BASE_URL', None), network.icon.url)
+        graph.add((resource_uri, FOAF.depiction, Literal(network_icon_url)))
+
+    insert_by_post(graph)
+
+
+def delete_network_rdf(network):
+    resource_uri = resource_uri_for_network_from_slug(network.slug)
 
     delete_resource(resource_uri)
