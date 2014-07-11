5eff7ec619fffb0a78d3ba6023c9e8573ce5c29e
Oscar <oscar.pdr@gmail.com>
2014-07-11 08:01:42 +0200

    New models for Contributions, Talks/Courses and Awards added

--- labman_ud/entities/utils/admin.py
+++ labman_ud/entities/utils/admin.py
@@ -98,6 +98,207 @@ class PhDProgramAdmin(admin.ModelAdmin):
 
 
 ###########################################################################
+# Class: LicenseAdmin
+###########################################################################
+
+class LicenseAdmin(admin.ModelAdmin):
+    model = License
+
+
+###########################################################################
+# Class: FileItemAdmin
+###########################################################################
+
+class FileItemAdmin(admin.ModelAdmin):
+    model = FileItem
+
+
+###########################################################################
+# Class: FileItemInline
+###########################################################################
+
+class FileItemInline(admin.TabularInline):
+    model = FileItem
+    extra = 1
+
+
+###########################################################################
+# Class: PersonRelatedToContributionInline
+###########################################################################
+
+class PersonRelatedToContributionInline(admin.TabularInline):
+    model = PersonRelatedToContribution
+    extra = 1
+
+
+###########################################################################
+# Class: PublicationRelatedToContributionInline
+###########################################################################
+
+class PublicationRelatedToContributionInline(admin.TabularInline):
+    model = PublicationRelatedToContribution
+    extra = 1
+
+
+###########################################################################
+# Class: ProjectRelatedToContributionInline
+###########################################################################
+
+class ProjectRelatedToContributionInline(admin.TabularInline):
+    model = ProjectRelatedToContribution
+    extra = 1
+
+
+###########################################################################
+# Class: TagRelatedToContributionInline
+###########################################################################
+
+class TagRelatedToContributionInline(admin.TabularInline):
+    model = TagRelatedToContribution
+    extra = 1
+
+
+###########################################################################
+# Class: FileItemRelatedToContributionInline
+###########################################################################
+
+class FileItemRelatedToContributionInline(admin.TabularInline):
+    model = FileItemRelatedToContribution
+    extra = 1
+
+
+###########################################################################
+# Class: ContributionTypeAdmin
+###########################################################################
+
+class ContributionTypeAdmin(admin.ModelAdmin):
+    model = ContributionType
+
+
+###########################################################################
+# Class: ContributionAdmin
+###########################################################################
+
+class ContributionAdmin(admin.ModelAdmin):
+    model = Contribution
+
+    search_fields = ['title']
+    list_display = ['title', 'contribution_type']
+    exclude = [
+        'slug',
+    ]
+    inlines = [
+        FileItemRelatedToContributionInline,
+        PersonRelatedToContributionInline,
+        ProjectRelatedToContributionInline,
+        PublicationRelatedToContributionInline,
+        TagRelatedToContributionInline,
+    ]
+
+
+###########################################################################
+# Class: PersonRelatedToTalkOrCourseInline
+###########################################################################
+
+class PersonRelatedToTalkOrCourseInline(admin.TabularInline):
+    model = PersonRelatedToTalkOrCourse
+    extra = 1
+
+
+###########################################################################
+# Class: ProjectRelatedToTalkOrCourseInline
+###########################################################################
+
+class ProjectRelatedToTalkOrCourseInline(admin.TabularInline):
+    model = ProjectRelatedToTalkOrCourse
+    extra = 1
+
+
+###########################################################################
+# Class: TagRelatedToTalkOrCourseInline
+###########################################################################
+
+class TagRelatedToTalkOrCourseInline(admin.TabularInline):
+    model = TagRelatedToTalkOrCourse
+    extra = 1
+
+
+###########################################################################
+# Class: FileItemRelatedToTalkOrCourseInline
+###########################################################################
+
+class FileItemRelatedToTalkOrCourseInline(admin.TabularInline):
+    model = FileItemRelatedToTalkOrCourse
+    extra = 1
+
+
+###########################################################################
+# Class: TalkOrCourseAdmin
+###########################################################################
+
+class TalkOrCourseAdmin(admin.ModelAdmin):
+    model = TalkOrCourse
+
+    search_fields = ['title']
+    list_display = ['title', 'start_date', 'end_date']
+    exclude = [
+        'slug',
+    ]
+    inlines = [
+        FileItemRelatedToTalkOrCourseInline,
+        PersonRelatedToTalkOrCourseInline,
+        ProjectRelatedToTalkOrCourseInline,
+        TagRelatedToTalkOrCourseInline,
+    ]
+
+
+###########################################################################
+# Class: PersonRelatedToAwardInline
+###########################################################################
+
+class PersonRelatedToAwardInline(admin.TabularInline):
+    model = PersonRelatedToAward
+    extra = 1
+
+
+###########################################################################
+# Class: ProjectRelatedToAwardInline
+###########################################################################
+
+class ProjectRelatedToAwardInline(admin.TabularInline):
+    model = ProjectRelatedToAward
+    extra = 1
+
+
+###########################################################################
+# Class: PublicationRelatedToAwardInline
+###########################################################################
+
+class PublicationRelatedToAwardInline(admin.TabularInline):
+    model = PublicationRelatedToAward
+    extra = 1
+
+
+###########################################################################
+# Class: AwardAdmin
+###########################################################################
+
+class AwardAdmin(admin.ModelAdmin):
+    model = Award
+
+    search_fields = ['full_name']
+    list_display = ['full_name', 'date']
+    exclude = [
+        'slug',
+    ]
+    inlines = [
+        PublicationRelatedToAwardInline,
+        PersonRelatedToAwardInline,
+        ProjectRelatedToAwardInline,
+    ]
+
+
+###########################################################################
 ###########################################################################
 # Register classes
 ###########################################################################
@@ -111,3 +312,9 @@ admin.site.register(Network, NetworkAdmin)
 admin.site.register(PhDProgram, PhDProgramAdmin)
 admin.site.register(Role, RoleAdmin)
 admin.site.register(Tag, TagAdmin)
+admin.site.register(FileItem, FileItemAdmin)
+admin.site.register(License, LicenseAdmin)
+admin.site.register(ContributionType, ContributionTypeAdmin)
+admin.site.register(Contribution, ContributionAdmin)
+admin.site.register(TalkOrCourse, TalkOrCourseAdmin)
+admin.site.register(Award, AwardAdmin)
--- labman_ud/entities/utils/models.py
+++ labman_ud/entities/utils/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.template.defaultfilters import slugify
 from entities.core.models import BaseModel
 
+from redactor.fields import RedactorField
 from .linked_data import *
 
 # Create your models here.
@@ -13,6 +14,10 @@ from .linked_data import *
 
 def network_icon_path(self, filename):
     return "%s/%s%s" % ("networks", self.slug, os.path.splitext(filename)[-1])
+
+
+def file_path(self, filename):
+    return "%s/%s%s" % ("files", self.slug, os.path.splitext(filename)[-1])
 
 
 ###########################################################################
@@ -293,3 +298,378 @@ class PhDProgram(BaseModel):
         blank=True,
         null=True,
     )
+
+
+###########################################################################
+# Model: ContributionType
+###########################################################################
+
+class ContributionType(BaseModel):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    slug = models.SlugField(
+        max_length=100,
+        blank=True,
+        unique=True,
+    )
+
+    description = models.TextField(
+        max_length=1500,
+        blank=True,
+    )
+
+    class Meta:
+        ordering = ['slug']
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(str(self.name.encode('utf-8')))
+        super(ContributionType, self).save(*args, **kwargs)
+
+
+###########################################################################
+# Model: Contribution
+###########################################################################
+
+class Contribution(BaseModel):
+    title = models.CharField(
+        max_length=250,
+    )
+
+    slug = models.SlugField(
+        max_length=250,
+        blank=True,
+        null=True,
+    )
+
+    contribution_type = models.ForeignKey('ContributionType')
+
+    description = RedactorField()
+
+    license = models.ForeignKey(
+        'License',
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        ordering = ['slug']
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(str(self.name.encode('utf-8')))
+        super(Contribution, self).save(*args, **kwargs)
+
+
+###########################################################################
+# Model: PersonRelatedToContribution
+###########################################################################
+
+class PersonRelatedToContribution(BaseModel):
+    person = models.ForeignKey('persons.Person')
+    contribution = models.ForeignKey('Contribution')
+
+    def __unicode__(self):
+        return u'%s related contribution:%s' % (self.person.full_name, self.contribution.title)
+
+
+###########################################################################
+# Model: ProjectRelatedToContribution
+###########################################################################
+
+class ProjectRelatedToContribution(BaseModel):
+    project = models.ForeignKey('projects.Project')
+    contribution = models.ForeignKey('Contribution')
+
+    def __unicode__(self):
+        return u'%s related contribution:%s' % (self.project.full_name, self.contribution.title)
+
+
+###########################################################################
+# Model: PublicationRelatedToContribution
+###########################################################################
+
+class PublicationRelatedToContribution(BaseModel):
+    publication = models.ForeignKey('publications.Publication')
+    contribution = models.ForeignKey('Contribution')
+
+    def __unicode__(self):
+        return u'%s related contribution:%s' % (self.publication.title, self.contribution.title)
+
+
+###########################################################################
+# Model: TagRelatedToContribution
+###########################################################################
+
+class TagRelatedToContribution(BaseModel):
+    tag = models.ForeignKey('Tag')
+    contribution = models.ForeignKey('Contribution')
+
+    def __unicode__(self):
+        return u'%s related contribution:%s' % (self.tag.name, self.contribution.title)
+
+
+###########################################################################
+# Model: FileItemRelatedToContribution
+###########################################################################
+
+class FileItemRelatedToContribution(BaseModel):
+    file_item = models.ForeignKey('FileItem')
+    contribution = models.ForeignKey('Contribution')
+
+    def __unicode__(self):
+        return u'%s related contribution:%s' % (self.file_item.name, self.contribution.title)
+
+
+###########################################################################
+# Model: FileItem
+###########################################################################
+
+class FileItem(BaseModel):
+    name = models.CharField(
+        max_length=250,
+    )
+
+    slug = models.SlugField(
+        max_length=250,
+        blank=True,
+        null=True,
+    )
+
+    description = models.TextField(
+        max_length=1500,
+        blank=True,
+    )
+
+    logo = models.FileField(
+        upload_to=file_path,
+        blank=True,
+        null=True,
+    )
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+
+        super(FileItem, self).save(*args, **kwargs)
+
+
+###########################################################################
+# Model: License
+###########################################################################
+
+class License(BaseModel):
+    full_name = models.CharField(
+        max_length=150,
+    )
+
+    short_name = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+    )
+
+    slug = models.SlugField(
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+
+    license_url = models.URLField()
+
+    def __unicode__(self):
+        return u'%s' % (self.full_name)
+
+    def save(self, *args, **kwargs):
+        if not self.short_name:
+            self.short_name = self.full_name
+
+        self.slug = slugify(self.full_name)
+
+        super(License, self).save(*args, **kwargs)
+
+
+###########################################################################
+# Model: TalkOrCourse
+###########################################################################
+
+class TalkOrCourse(BaseModel):
+    title = models.CharField(
+        max_length=250,
+    )
+
+    slug = models.SlugField(
+        max_length=250,
+        blank=True,
+        null=True,
+    )
+
+    description = RedactorField()
+
+    start_date = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    end_date = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    event = models.ForeignKey(
+        'events.Event',
+        blank=True,
+        null=True,
+    )
+
+    def __unicode__(self):
+        return u'%s' % (self.title)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+
+        super(TalkOrCourse, self).save(*args, **kwargs)
+
+
+###########################################################################
+# Model: PersonRelatedToTalkOrCourse
+###########################################################################
+
+class PersonRelatedToTalkOrCourse(BaseModel):
+    person = models.ForeignKey('persons.Person')
+    talk_or_course = models.ForeignKey('TalkOrCourse')
+
+    def __unicode__(self):
+        return u'%s related talk or course:%s' % (self.person.full_name, self.talk_or_course.title)
+
+
+###########################################################################
+# Model: ProjectRelatedToTalkOrCourse
+###########################################################################
+
+class ProjectRelatedToTalkOrCourse(BaseModel):
+    project = models.ForeignKey('projects.Project')
+    talk_or_course = models.ForeignKey('TalkOrCourse')
+
+    def __unicode__(self):
+        return u'%s related talk or course:%s' % (self.project.full_name, self.talk_or_course.title)
+
+
+###########################################################################
+# Model: TagRelatedToTalkOrCourse
+###########################################################################
+
+class TagRelatedToTalkOrCourse(BaseModel):
+    tag = models.ForeignKey('Tag')
+    talk_or_course = models.ForeignKey('TalkOrCourse')
+
+    def __unicode__(self):
+        return u'%s related talk or course:%s' % (self.tag.name, self.talk_or_course.title)
+
+
+###########################################################################
+# Model: FileItemRelatedToTalkOrCourse
+###########################################################################
+
+class FileItemRelatedToTalkOrCourse(BaseModel):
+    file_item = models.ForeignKey('FileItem')
+    talk_or_course = models.ForeignKey('TalkOrCourse')
+
+    def __unicode__(self):
+        return u'%s related talk or course:%s' % (self.file_item.name, self.talk_or_course.title)
+
+
+###########################################################################
+# Model: Award
+###########################################################################
+
+class Award(BaseModel):
+    full_name = models.CharField(
+        max_length=250,
+    )
+
+    short_name = models.CharField(
+        max_length=250,
+        blank=True,
+        null=True,
+    )
+
+    slug = models.SlugField(
+        max_length=250,
+        blank=True,
+        null=True,
+    )
+
+    description = RedactorField()
+
+    date = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    event = models.ForeignKey(
+        'events.Event',
+        blank=True,
+        null=True,
+    )
+
+    supporting_organization = models.ForeignKey(
+        'organizations.Organization',
+        blank=True,
+        null=True,
+    )
+
+    def __unicode__(self):
+        return u'%s' % (self.full_name)
+
+    def save(self, *args, **kwargs):
+        if not self.short_name:
+            self.short_name = self.full_name
+
+        self.slug = slugify(self.full_name)
+
+        super(Award, self).save(*args, **kwargs)
+
+
+###########################################################################
+# Model: PersonRelatedToAward
+###########################################################################
+
+class PersonRelatedToAward(BaseModel):
+    person = models.ForeignKey('persons.Person')
+    award = models.ForeignKey('Award')
+
+    def __unicode__(self):
+        return u'%s related talk or course:%s' % (self.person.full_name, self.award.full_name)
+
+
+###########################################################################
+# Model: ProjectRelatedToAward
+###########################################################################
+
+class ProjectRelatedToAward(BaseModel):
+    project = models.ForeignKey('projects.Project')
+    award = models.ForeignKey('Award')
+
+    def __unicode__(self):
+        return u'%s related talk or course:%s' % (self.project.full_name, self.award.full_name)
+
+
+###########################################################################
+# Model: PublicationRelatedToAward
+###########################################################################
+
+class PublicationRelatedToAward(BaseModel):
+    publication = models.ForeignKey('publications.Publication')
+    award = models.ForeignKey('Award')
+
+    def __unicode__(self):
+        return u'%s related talk or course:%s' % (self.publication.title, self.award.full_name)
