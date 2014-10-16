2e927fdcd6f6781e2a653c145d9c71216a2c451e
Oscar <oscar.pdr@gmail.com>
2014-10-16 15:18:04 +0200

    Emptied key parameter to parse single publication

--- labman_ud/maintenance_tasks/publications/parse_publication.py
+++ labman_ud/maintenance_tasks/publications/parse_publication.py
@@ -9,4 +9,4 @@ from extractors.zotero.zotero_extractor import generate_publication_from_zotero
 ###########################################################################
 
 def parse_publication():
-    generate_publication_from_zotero('NZK77RXD')
+    generate_publication_from_zotero('')
