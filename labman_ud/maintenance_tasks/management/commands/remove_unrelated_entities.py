aa954aa959e88571ea9e5b68a77b7549ab754132
Oscar <oscar.pdr@gmail.com>
2014-10-17 14:19:31 +0200

    Removed script which caused apparently unrelated persons to be removed from the system

--- labman_ud/maintenance_tasks/management/commands/remove_unrelated_entities.py
+++ labman_ud/maintenance_tasks/management/commands/remove_unrelated_entities.py
@@ -2,7 +2,6 @@
 
 from django.core.management.base import NoArgsCommand
 
-from maintenance_tasks.general.remove_unrelated_people import remove_unrelated_people
 from maintenance_tasks.general.remove_unrelated_tags import remove_unrelated_tags
 
 
@@ -12,5 +11,4 @@ class Command(NoArgsCommand):
     help = 'Removes entities not related to any other model instance'
 
     def handle_noargs(self, **options):
-        remove_unrelated_people()
         remove_unrelated_tags()
