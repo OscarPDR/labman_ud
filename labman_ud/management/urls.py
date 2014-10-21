0398ae1cd9adf5a83fa4f9e0e3843aa6d5b034ef
Oscar <oscar.pdr@gmail.com>
2014-10-21 16:52:43 +0200

    Fixed bugs

--- labman_ud/management/urls.py
+++ labman_ud/management/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^similarity_ratio/default/$', 'management.views.check_names_similarity', name='check_names_similarity_default'),
     url(r'^alias/(?P<person_id>\d+)/(?P<alias_id>\d+)/(?P<threshold_ratio>\d{2})/$', 'management.views.assign_alias', name='assign_alias'),
     url(r'^ignore_relationship/(?P<test_person_id>\d+)/(?P<testing_person_id>\d+)/(?P<threshold_ratio>\d{2})/$', 'management.views.ignore_relationship', name='ignore_relationship'),
-    url(r'^reset_ignored_relationships/$', 'management.views.reset_ignored_relationships', name='reset_ignored_relationships'),
+    url(r'^reset_ignored_relationships/(?P<threshold_ratio>\d{2})/$', 'management.views.reset_ignored_relationships', name='reset_ignored_relationships'),
 ]
