8def19d21fbcc18b8c6a52551ce6f891d0f9a62a
Oscar <oscar.pdr@gmail.com>
2014-10-23 15:26:55 +0200

    Settings always available from database

--- labman_ud/labman_ud/context_processors.py
+++ labman_ud/labman_ud/context_processors.py
@@ -1,7 +1,59 @@
-# -*- encoding: utf-8 -*-
 
 from django.conf import settings
 
+from labman_setup.models import *
+
 
 def global_vars(request):
-    return {'RDF_URI': getattr(settings, 'GRAPH_BASE_URL', None) + '/'}
+
+    _settings = LabmanDeployGeneralSettings.objects.get()
+    social_profiles = OfficialSocialProfile.objects.all().order_by('name')
+
+    footer_sections = 0
+    address_details = False
+    contact_details = False
+    social_details = False
+
+    if (_settings.address):
+        footer_sections += 1
+        address_details = True
+
+    if (_settings.email_address or _settings.contact_person or _settings.phone_number):
+        footer_sections += 1
+        contact_details = True
+
+    if (len(social_profiles) > 0):
+        footer_sections += 1
+        social_details = True
+
+    if footer_sections in (0, 1):
+        footer_divisions_width = 12
+
+    elif footer_sections == 2:
+        footer_divisions_width = 6
+
+    else:
+        footer_divisions_width = 4
+
+    if (len(social_profiles) <= 3):
+        social_profile_width = 3
+
+    elif (len(social_profiles) <= 5):
+        social_profile_width = 2
+
+    else:
+        social_profile_width = 1
+
+    return_dict = {
+        'RESEARCH_GROUP_SETTINGS': _settings,
+        'RDF_URI': getattr(settings, 'GRAPH_BASE_URL', None) + '/',
+        'TEAM_IMAGE_PATH': getattr(settings, 'TEAM_IMAGE_PATH', None) + '',
+        'FOOTER_DIVISIONS_WIDTH': footer_divisions_width,
+        'ADDRESS_DETAILS': address_details,
+        'CONTACT_DETAILS': contact_details,
+        'SOCIAL_DETAILS': social_details,
+        'SOCIAL_PROFILES': social_profiles,
+        'SOCIAL_PROFILE_WIDTH': social_profile_width,
+    }
+
+    return return_dict
